a642d349d85a82d105788c14c4e4f07368dfc8c1
sublun <sublun@126.com>
2018-10-21 19:51:53 +0800

    Merge branch 'dev'
    # Conflicts:
    #	src/com/company/Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,6 +9,7 @@ public class Main {
         System.out.println("hello  shijie");
         System.out.println("第一比");
         System.out.println("第一");
+        System.out.println("die");
 
     }
 }
